f8f7711d8f8aabfa2fca379c79e9bd9beaff7cba
razzalok <alok.raj@mindfire.com>
2026-01-15 13:01:24 +0530

    feat(client): remove heading

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App() {
 
   return (
     <>
-    <p>Hello</p>
-    <h1>Hello</h1>
+    
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
